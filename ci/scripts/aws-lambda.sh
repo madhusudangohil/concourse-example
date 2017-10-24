ae8f8d1bd2dc7a4c0675a05cac282a4b8ca878e3
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-10-24 14:21:06 -0400

    a

--- ci/scripts/aws-lambda.sh
+++ ci/scripts/aws-lambda.sh
@@ -4,16 +4,15 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-cd ~
-mkdir .aws
-echo '[profile dev]
+mkdir ~/.aws
+echo '[profile default]
 region = us-west-2
 role_arn = arn:aws:iam::300813158921:role/GroupAccess-Developers-Channels
-source_profile = dev' > config
+source_profile = default' > config
 
-echo '[dev]
+echo '[default]
 aws_access_key_id=ASIAI2UWEB7L5ZZ2FRBA
 aws_secret_access_key=x/F+zycN1IgY6mRX7YCxsOjVRBtRImNLsqgyGmyZ
 aws_session_token=FQoDYXdzEDAaDG1bXew8aH6YV5jblyKwAZagx4IJtEDaUmcJsCE2y8tH8yGlGrGLzmc1eX4yJEuN/9zv5mZL00fkbBUEu+ZhwdFDTLB6FMX0/Aa7sMh/Vzf3rpzPNQ0+TX89SIwu/f+nkfR97QkLmL7iOLKVzwVz23/1zucHJFCkRC6S8lYdDV6LEbwmuQDrfarNCokjoenIz2GEyuV1IDkc5AWbN+qQRMIMNaqrA95pSAs0iJUTj/ZQgVViSU/KMPQmgiMGt46kKNmpvc8F' > credentials
 
-aws lambda list-functions --profile dev --region us-west-2
+aws lambda list-functions
