792073740706d7f66fe737eaea536f79edb78cca
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-10-24 11:14:50 -0400

    c

--- ci/scripts/aws-lambda.sh
+++ ci/scripts/aws-lambda.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-aws lambda list-functions --profile dev --region us-west-2
+docker run -it -v $HOME/.aws:/home/aws/.aws berrycloud/maven-aws-cli-node aws lambda list-functions --profile dev --region us-west-2
