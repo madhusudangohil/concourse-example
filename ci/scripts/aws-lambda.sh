bf9bbbaf77207a8a177f57321686472d122f2db4
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-10-24 14:15:07 -0400

    g

--- ci/scripts/aws-lambda.sh
+++ ci/scripts/aws-lambda.sh
@@ -4,4 +4,16 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-docker run -it -v $HOME/.aws:/home/aws/.aws berrycloud/maven-aws-cli-node aws lambda list-functions --profile dev --region us-west-2
+cd ~
+mkdir .aws
+echo '[profile dev]
+region = us-west-2
+role_arn = arn:aws:iam::300813158921:role/GroupAccess-Developers-Channels
+source_profile = dev' > config
+
+echo '[dev]
+aws_access_key_id=ASIAI2UWEB7L5ZZ2FRBA
+aws_secret_access_key=x/F+zycN1IgY6mRX7YCxsOjVRBtRImNLsqgyGmyZ
+aws_session_token=FQoDYXdzEDAaDG1bXew8aH6YV5jblyKwAZagx4IJtEDaUmcJsCE2y8tH8yGlGrGLzmc1eX4yJEuN/9zv5mZL00fkbBUEu+ZhwdFDTLB6FMX0/Aa7sMh/Vzf3rpzPNQ0+TX89SIwu/f+nkfR97QkLmL7iOLKVzwVz23/1zucHJFCkRC6S8lYdDV6LEbwmuQDrfarNCokjoenIz2GEyuV1IDkc5AWbN+qQRMIMNaqrA95pSAs0iJUTj/ZQgVViSU/KMPQmgiMGt46kKNmpvc8F' > credentials
+
+aws lambda list-functions --profile dev --region us-west-2
