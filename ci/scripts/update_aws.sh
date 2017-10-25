821ddf8f23a11e77933dbf58dc80bdc39a86e290
madhusudan gohil <madhusudan.gohil@incontact.com>
2017-10-25 12:56:17 -0400

    g

--- ci/scripts/update_aws.sh
+++ ci/scripts/update_aws.sh
@@ -4,14 +4,16 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+mkdir ~/.aws
+
 echo '[profile default]
 region = us-west-2
 role_arn = arn:aws:iam::300813158921:role/GroupAccess-Developers-Channels
-source_profile = default' > config
+source_profile = default' > ~/.aws/config
 
 echo '[default]
 aws_access_key_id=ASIAIN4O3NH73IWMPRXQ
 aws_secret_access_key=iiSyV0OIPicD4Pi+RuuGdF7RpOaBmeM6p0J0JSBt
-aws_session_token=FQoDYXdzEEcaDJmhvAC42AL3bgxVXyKwAYlvr/yI0z3s4WR8kMaidKxcxmhADVS8rwboWM/TQ7TVnoaHpg/e4keIrKJcbn9kArzhLRYl50MGzH85jB64AKsczjqP852rWM2wgs6oL4NGcniCXsMM22dTce6rFdtaPj03solgij/w0pWgaGmu0YWtHzVNb3mQ0s0yiP0ngNFvPLStJ4Rw6/AwTORZSJZb1Euw+Jj/DocCTVnpwzutR5OmZNF4R02HcjKmlNYxzC76KKS6ws8F' > credentials
+aws_session_token=FQoDYXdzEEcaDJmhvAC42AL3bgxVXyKwAYlvr/yI0z3s4WR8kMaidKxcxmhADVS8rwboWM/TQ7TVnoaHpg/e4keIrKJcbn9kArzhLRYl50MGzH85jB64AKsczjqP852rWM2wgs6oL4NGcniCXsMM22dTce6rFdtaPj03solgij/w0pWgaGmu0YWtHzVNb3mQ0s0yiP0ngNFvPLStJ4Rw6/AwTORZSJZb1Euw+Jj/DocCTVnpwzutR5OmZNF4R02HcjKmlNYxzC76KKS6ws8F' > ~/.aws/credentials
 
 ls
