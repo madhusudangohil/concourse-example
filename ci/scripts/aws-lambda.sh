65bd5b967851b6ec4597b4a808cb833b0e24ac62
Madhusudan <ms_gohil@yahoo.com>
2017-10-25 15:16:56 -0400

    removing keys

--- ci/scripts/aws-lambda.sh
+++ ci/scripts/aws-lambda.sh
@@ -10,13 +10,13 @@ mkdir ~/.aws
 cd ~/.aws
 echo '[profile default]
 region = us-west-2
-role_arn = arn:aws:iam::300813158921:role/GroupAccess-Developers-Channels
+role_arn = 
 source_profile = default' > config
 
 echo '[default]
-aws_access_key_id=ASIAIN4O3NH73IWMPRXQ
-aws_secret_access_key=iiSyV0OIPicD4Pi+RuuGdF7RpOaBmeM6p0J0JSBt
-aws_session_token=FQoDYXdzEEcaDJmhvAC42AL3bgxVXyKwAYlvr/yI0z3s4WR8kMaidKxcxmhADVS8rwboWM/TQ7TVnoaHpg/e4keIrKJcbn9kArzhLRYl50MGzH85jB64AKsczjqP852rWM2wgs6oL4NGcniCXsMM22dTce6rFdtaPj03solgij/w0pWgaGmu0YWtHzVNb3mQ0s0yiP0ngNFvPLStJ4Rw6/AwTORZSJZb1Euw+Jj/DocCTVnpwzutR5OmZNF4R02HcjKmlNYxzC76KKS6ws8F' > credentials
+aws_access_key_id=
+aws_secret_access_key=
+aws_session_token=' > credentials
 
 cd - 
 cd project-src
